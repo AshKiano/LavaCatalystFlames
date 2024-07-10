e202790dbf9f9b4ac0490fd3abb0640a3cab1535
jvyskocil <ash.kiano@gmail.com>
2024-07-11 00:59:45 +0200

    version 1.4

--- src/main/java/com/ashkiano/lavacatalystflames/LavaCatalystFlames.java
+++ src/main/java/com/ashkiano/lavacatalystflames/LavaCatalystFlames.java
@@ -72,7 +72,7 @@ public class LavaCatalystFlames extends JavaPlugin implements Listener {
     private void checkForUpdates() {
         try {
             String pluginName = this.getDescription().getName();
-            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
 
